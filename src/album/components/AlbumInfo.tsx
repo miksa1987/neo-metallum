363a599fc048cfdcb5e1a8d58aa1d83b17490862
Mika <mika87@gmail.com>
2020-03-15 19:26:35 +0200

    Cleaned AlbumInfo

--- src/album/components/AlbumInfo.tsx
+++ src/album/components/AlbumInfo.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import { AlbumType } from '../../common/types';
 import { Section } from '../../common';
 
@@ -7,18 +6,13 @@ interface Props {
   album: AlbumType
 }
 
-const Layout = styled(Section)`
-  display: flex;
-  flex-direction: column;
-`;
-
 export const AlbumInfo: React.FC<Props> = ({ album }) => {
   return (
-    <Layout>
+    <Section>
       <p>Released in: {album.releaseDate}</p>
       <p>Type: {album.type}</p>
       <p>Format: {album.format}</p>
       <p>Label: {album.label}</p>
-    </Layout>
+    </Section>
   );
 }
